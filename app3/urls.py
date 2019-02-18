491e61351d0fc1fa31b392c01b3e27c45f881bfd
unknown <tianzi@vt.edu>
2019-02-17 20:05:05 -0500

    moving points

--- app3/urls.py
+++ app3/urls.py
@@ -5,5 +5,7 @@ from app3.views import IndexView,TaskGenerationView
 
 urlpatterns = [
     path('', TaskGenerationView.as_view()),
-    path('/members', IndexView.as_view()),
+    path('members', IndexView.as_view()),
+    path('edit',TemplateView.as_view(template_name="app3/edit.html")),
+    path('sketch',TemplateView.as_view(template_name="app3/sketch.html")),
 ]
